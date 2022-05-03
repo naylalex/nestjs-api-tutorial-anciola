4a7f2300f91fbd45e7e6c6fdd488acb5ce1c164e
anciola <90790959+anciola@users.noreply.github.com>
2022-05-03 23:46:31 +0000

    y

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('tasks')
 @Controller('tasks')
+@Get
+getTasks() {
+    return null
+}
+@Get(':id')
+getTaskById(){
+    return null
+}
+@Post()
+CreateTask(){
+    return null
+}
+
 export class TasksController {}
