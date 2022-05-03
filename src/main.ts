7de3bd7189138abe13db32b58e6f3250fdb8a413
anciola <90790959+anciola@users.noreply.github.com>
2022-05-03 23:42:40 +0000

    a

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('TC401 API')
+    .setDescription('sample API')
+    .setVersion('0.1')
+    .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+    
+
   await app.listen(3000);
 }
 bootstrap();
